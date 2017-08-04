09b818d9655c6effbde0ae91e1d494ea3337bdbd
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-08-04 11:12:08 -0400

    Add graphql mutation

--- server.js
+++ server.js
@@ -1,25 +1,59 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql'
+import {
+  GraphQLSchema, GraphQLObjectType, GraphQLString,
+  GraphQLNonNull, GraphQLID
+} from 'graphql'
 
 
 const app = express()
 
+// Query
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
   description: 'The root query',
   fields: {
-    viewer: {
+    node: {
       type: GraphQLString,
-      resolve () {
-        return 'viewer!'
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve (source, args) {
+        return inMemoryStore[args.id]
+      }
+    }
+  }
+})
+
+// Mutation
+let inMemoryStore = {}
+const RootMutation = new GraphQLObjectType({
+  name: 'RootMutation',
+  description: 'The root mutation',
+  fields: {
+    setNode: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        },
+        value: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve (source, args) {
+        inMemoryStore[args.id] = args.value
+        return inMemoryStore[args.id]
       }
     }
   }
 })
 
 const Schema = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: RootMutation
 })
 
 app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }))
