cc262c47ce52c079ed38a4780898e0aa93d24ef4
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-08-08 11:14:33 -0400

    Add dataloader

--- src/database.js
+++ src/database.js
@@ -6,7 +6,7 @@ export const db = new sqlite3.Database('./db.sqlite')
 
 export const getSql = (query) => {
   return new Promise((resolve, reject) => {
-    // console.log(query.text)
+    console.log(query.text)
     // console.log(query.values)
     db.all(query.text, query.values, (err, rows) => {
       if (err) {
